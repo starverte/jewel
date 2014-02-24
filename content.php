ca5f48a711484aa29ef169c33711db82b386f924
Matt Beall <mbeall@starverte.com>
2014-02-23 23:10:55 -0700

    Update spacing and drop subpackage tag

--- content.php
+++ content.php
@@ -1,60 +1,59 @@
 <?php
 /**
- * @package WordPress
- * @subpackage Jewel
+ * @package Jewel
  */
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<h1 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'jewel' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
-		<?php edit_post_link( __( 'Edit', 'jewel' ), '<span class="edit-link">', '</span>' ); ?>
-		<?php if ( 'post' == get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php jewel_posted_on(); ?>
-		</div><!-- .entry-meta -->
-		<?php endif; ?>
-	</header><!-- .entry-header -->
+  <header class="entry-header">
+    <h1 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'jewel' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
+    <?php edit_post_link( __( 'Edit', 'jewel' ), '<span class="edit-link">', '</span>' ); ?>
+    <?php if ( 'post' == get_post_type() ) : ?>
+    <div class="entry-meta">
+      <?php jewel_posted_on(); ?>
+    </div><!-- .entry-meta -->
+    <?php endif; ?>
+  </header><!-- .entry-header -->
 
-	<?php if ( is_search() ) : // Only display Excerpts for Search ?>
-	<div class="entry-summary">
-		<?php the_excerpt(); ?>
-	</div><!-- .entry-summary -->
-	<?php else : ?>
-	<div class="entry-content">
-		<?php the_content( __( 'More Info', 'jewel' ) ); ?>
-		<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'jewel' ), 'after' => '</div>' ) ); ?>
-	</div><!-- .entry-content -->
-	<?php endif; ?>
+  <?php if ( is_search() ) : // Only display Excerpts for Search ?>
+  <div class="entry-summary">
+    <?php the_excerpt(); ?>
+  </div><!-- .entry-summary -->
+  <?php else : ?>
+  <div class="entry-content">
+    <?php the_content( __( 'More Info', 'jewel' ) ); ?>
+    <?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'jewel' ), 'after' => '</div>' ) ); ?>
+  </div><!-- .entry-content -->
+  <?php endif; ?>
 
-	<footer class="entry-meta">
-		<?php if ( 'post' == get_post_type() ) : // Hide category and tag text for pages on Search ?>
-			<?php
-				/* translators: used between list items, there is a space after the comma */
-				$categories_list = get_the_category_list( __( ', ', 'jewel' ) );
-				if ( $categories_list && jewel_categorized_blog() ) :
-			?>
-			<span class="cat-links">
-				<?php printf( __( 'Posted in %1$s', 'jewel' ), $categories_list ); ?>
-			</span>
-			<span class="sep"> | </span>
-			<?php endif; // End if categories ?>
+  <footer class="entry-meta">
+    <?php if ( 'post' == get_post_type() ) : // Hide category and tag text for pages on Search ?>
+      <?php
+        /* translators: used between list items, there is a space after the comma */
+        $categories_list = get_the_category_list( __( ', ', 'jewel' ) );
+        if ( $categories_list && jewel_categorized_blog() ) :
+      ?>
+      <span class="cat-links">
+        <?php printf( __( 'Posted in %1$s', 'jewel' ), $categories_list ); ?>
+      </span>
+      <span class="sep"> | </span>
+      <?php endif; // End if categories ?>
 
-			<?php
-				/* translators: used between list items, there is a space after the comma */
-				$tags_list = get_the_tag_list( '', __( ', ', 'jewel' ) );
-				if ( $tags_list ) :
-			?>
-			<span class="tag-links">
-				<?php printf( __( 'Tagged %1$s', 'jewel' ), $tags_list ); ?>
-			</span>
-			<span class="sep"> | </span>
-			<?php endif; // End if $tags_list ?>
-		<?php endif; // End if 'post' == get_post_type() ?>
+      <?php
+        /* translators: used between list items, there is a space after the comma */
+        $tags_list = get_the_tag_list( '', __( ', ', 'jewel' ) );
+        if ( $tags_list ) :
+      ?>
+      <span class="tag-links">
+        <?php printf( __( 'Tagged %1$s', 'jewel' ), $tags_list ); ?>
+      </span>
+      <span class="sep"> | </span>
+      <?php endif; // End if $tags_list ?>
+    <?php endif; // End if 'post' == get_post_type() ?>
 
-		<?php if ( comments_open() || ( '0' != get_comments_number() && ! comments_open() ) ) : ?>
-		<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'jewel' ), __( '1 Comment', 'jewel' ), __( '% Comments', 'jewel' ) ); ?></span>
-		<span class="sep"> | </span>
-		<?php endif; ?>
-	</footer><!-- #entry-meta -->
+    <?php if ( comments_open() || ( '0' != get_comments_number() && ! comments_open() ) ) : ?>
+    <span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'jewel' ), __( '1 Comment', 'jewel' ), __( '% Comments', 'jewel' ) ); ?></span>
+    <span class="sep"> | </span>
+    <?php endif; ?>
+  </footer><!-- #entry-meta -->
 </article><!-- #post-<?php the_ID(); ?> -->
