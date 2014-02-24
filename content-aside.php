e36909a9522feeec81eef3f835e78b91a73d27ae
Matt Beall <mbeall@starverte.com>
2014-02-23 23:21:57 -0700

    Update spacing and drop subpackage tag

--- content-aside.php
+++ content-aside.php
@@ -4,34 +4,33 @@
  *
  * Learn more: http://codex.wordpress.org/Post_Formats
  *
- * @package WordPress
- * @subpackage Jewel
+ * @package Jewel
  * @since Jewel 1.0
  */
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<h1 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'jewel' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
-		<?php edit_post_link( __( 'Edit', 'jewel' ), '<span class="edit-link">', '</span>' ); ?>
-	</header><!-- .entry-header -->
-	
-	<?php if ( is_search() ) : // Only display Excerpts for search pages ?>
-	<div class="entry-summary">
-		<?php the_excerpt(); ?>
-	</div><!-- .entry-summary -->
-	<?php else : ?>
-	<div class="entry-content">
-		<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'jewel' ) ); ?>
-		<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'jewel' ), 'after' => '</div>' ) ); ?>
-	</div><!-- .entry-content -->
-	<?php endif; ?>
+  <header class="entry-header">
+    <h1 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'jewel' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
+    <?php edit_post_link( __( 'Edit', 'jewel' ), '<span class="edit-link">', '</span>' ); ?>
+  </header><!-- .entry-header -->
+  
+  <?php if ( is_search() ) : // Only display Excerpts for search pages ?>
+  <div class="entry-summary">
+    <?php the_excerpt(); ?>
+  </div><!-- .entry-summary -->
+  <?php else : ?>
+  <div class="entry-content">
+    <?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'jewel' ) ); ?>
+    <?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'jewel' ), 'after' => '</div>' ) ); ?>
+  </div><!-- .entry-content -->
+  <?php endif; ?>
 
-	<footer class="entry-meta">
-		<?php jewel_posted_on(); ?>
-		<?php if ( comments_open() || ( '0' != get_comments_number() && ! comments_open() ) ) : ?>
-		<span class="sep"> | </span>
-		<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'jewel' ), __( '1 Comment', 'jewel' ), __( '% Comments', 'jewel' ) ); ?></span>
-		<?php endif; ?>
-	</footer><!-- #entry-meta -->
+  <footer class="entry-meta">
+    <?php jewel_posted_on(); ?>
+    <?php if ( comments_open() || ( '0' != get_comments_number() && ! comments_open() ) ) : ?>
+    <span class="sep"> | </span>
+    <span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'jewel' ), __( '1 Comment', 'jewel' ), __( '% Comments', 'jewel' ) ); ?></span>
+    <?php endif; ?>
+  </footer><!-- #entry-meta -->
 </article><!-- #post-<?php the_ID(); ?> -->
